170bd241a08f7689bf764b6f61cd85e97018a7bc
Philipp Novaczek <p.novaczek@gmail.com>
2026-04-06 10:43:02 +0200

    initial fabric template

---
+++ src/client/java/pnovaczekdev/spellgems/client/SpellgemsClient.java
@@ -0,0 +1,10 @@
+package pnovaczekdev.spellgems.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpellgemsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/pnovaczekdev/spellgems/client/SpellgemsDataGenerator.java
@@ -0,0 +1,11 @@
+package pnovaczekdev.spellgems.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SpellgemsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
